ddae781fae5388e3ad1790b2f9c652f003be0076
akkaradet13 <akkaradet.pu.60@ubu.ac.th>
2020-10-28 15:53:14 +0700

    getdatafromclientandsave

--- user_example/models.py
+++ user_example/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    postName = models.CharField(max_length=30)
+    description = models.TextField()
+    time = models.CharField(max_length=30)
+    image = models.ImageField(upload_to='image', blank=True)
+
+    def __str__(self):
+        return self.postName
--- user_example/views.py
+++ user_example/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from user_example.models import Post
+from PIL import Image
 
 def index(request):
-    return render(request, 'user_example/index.html')
+    posts = Post.objects.all()
+
+    args = {'posts': posts}
+    return render(request, 'user_example/index.html', args)
+
+@csrf_exempt 
+def getData(request):
+    print('12346')
+    if request.method == 'POST':
+        print('post', request.POST)
+        print('file', request.FILES.get('upload_file'))
+        postName =  str(request.POST['postName'])
+        description = str(request.POST['description'])
+        time = str(request.POST['time'])
+        image = request.FILES.get('upload_file')
+        ins = Post(postName = postName, description = description, time = time, image = image)
+        ins.save()
+        print("saveData")
+        # im = Image.open(request.FILES.get('upload_file'))
+        # im.show()
+
+        return HttpResponse("Your response")
