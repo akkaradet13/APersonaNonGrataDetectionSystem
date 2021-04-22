330c170844aeda265f0ce5db17f5dc5a97ec0864
akkaradet13 <akkaradet.pu.60@ubu.ac.th>
2021-04-22 10:47:22 +0700

    edit return status

--- setting.py
+++ setting.py
@@ -2,6 +2,7 @@ from flask import Flask
 import RPi.GPIO as GPIO
 import time, os
 from playsound import playsound
+from flask_http_response import success, result, error
 
 app = Flask(__name__)
 
@@ -9,13 +10,15 @@ def relay(gpioControl):
     channel = 21
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(channel, GPIO.OUT)
-
-    GPIO.output(channel, GPIO.gpioControl)
+    if gpioControl == 'HIGH':
+        GPIO.output(channel, GPIO.HIGH)
+        GPIO.cleanup()
+    elif gpioControl == 'LOW':
+        GPIO.output(channel, GPIO.LOW)
     '''time.sleep(1)
     GPIO.output(channel, GPIO.LOW)'''
-    playSound('preview.mp3')
+    #playSound('preview.mp3')
     #time.sleep(3)
-    GPIO.cleanup()
 
 def playSound(file):
     os.system("mpg123 " + file)
@@ -27,12 +30,15 @@ def Home():
 @app.route('/actionDoor/<string:value>')
 def ActionDoor(value):
     if value == '0':
-        relay(HIGH)
+        relay('HIGH')
         #playSound('preview.mp3')
-        return 'open'
+        #return 'open'
     elif value == '1':
-        relay(LOW)
-        return 'close'
+        relay('LOW')
+        #return 'close'
+    f = open("./Actiondoor.txt", "w")
+    f.write(value)
+    f.close()
 
 @app.route('/levelSecurity/<string:value>')
 def SetLevelSecurity(value):
@@ -45,11 +51,12 @@ def SetLevelSecurity(value):
     return ("<h1>200</h1>")
 
 @app.route('/getValue/<string:value>')
-def GetValue(fileName):
-    f = open(f'./{fileName}.txt', "r")
+def GetValue(value):
+    f = open(f'./{value}.txt', "r")
     value = f.read()
+    print('value', value)
     f.close()
-    return value
+    return success.return_response(message=str(value), status=300)
     
 
 if __name__== '__main__':
